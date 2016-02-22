d0b7a23cf6f04aa04f51c334e694389bfc49780e
Alexander Afonyashin <kiosaku@mail.ru>
2016-02-22 15:55:04 +0300

    Direct parsing of query_string

--- hello.py
+++ hello.py
@@ -2,11 +2,13 @@ import urlparse
 
 def app(environ, start_response):
   response_body = ''
-  d = urlparse.parse_qs(environ['QUERY_STRING'])
-  for key, value in sorted(d.iteritems()):
-    value_join = ''.join(value)
+  a = environ['QUERY_STRING']
+  response_body = a.replace('&','\r\n')
+#  d = urlparse.parse_qs(environ['QUERY_STRING'])
+#  for key, value in sorted(d.iteritems()):
+#    value_join = ''.join(value)
 #    response_body += key + '=' + str(value) + '\n'
-    response_body += key + '=' + value_join + '\n'
+#    response_body += key + '=' + value_join + '\n'
   start_response('200 OK', [('Content-Type', 'text/html'), ('Content-Length', str(len(response_body)))])
   return[response_body]
 
