0ea11ae7946689c0d4c6be8d5bd8c4c3694d87a1
Alexander Afonyashin <kiosaku@mail.ru>
2016-03-23 07:06:52 +0000

    Add django dir structure

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.conf /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/ask.conf /etc/gunicorn.d/ask.py
 sudo /etc/init.d/gunicorn restart
